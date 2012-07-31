4235ea8f95ee3287ccf792baebe1916861be612b
Sam Panzer <panzer@google.com>
2012-07-30 18:33:40 -0700

    Finished array matcher - array-step-2

--- loop-convert/LoopActions.cpp
+++ loop-convert/LoopActions.cpp
@@ -5,25 +5,35 @@ namespace loop_migrate {
 
 using namespace clang::ast_matchers;
 
+const char LoopName[] = "forLoop";
+const char ConditionBoundName[] = "conditionBound";
+const char ConditionVarName[] = "conditionVar";
+const char IncrementVarName[] = "incrementVar";
+const char InitVarName[] = "initVar";
+
 void LoopPrinter::run(const MatchFinder::MatchResult &Result) {
   if (const ForStmt *FS = Result.Nodes.getStmtAs<ForStmt>("forLoop"))
     FS->dump();
 }
 
+static StatementMatcher ArrayLHSMatcher =
+  expression(ignoringImpCasts(declarationReference(to(
+      variable(hasType(isInteger())).bind(ConditionVarName)))));
+static StatementMatcher ArrayRHSMatcher =
+  expression(hasType(isInteger())).bind(ConditionBoundName);
+
 StatementMatcher LoopMatcher =
-  id("forLoop", forStmt(
+  id(LoopName, forStmt(
       hasLoopInit(declarationStatement(hasSingleDecl(variable(
-          hasInitializer(integerLiteral(equals(0))))))),
+          hasInitializer(integerLiteral(equals(0)))).bind(InitVarName)))),
       hasCondition(binaryOperator(hasOperatorName("<"),
-                                  hasRHS(declarationReference(to(
-                                      variable(hasType(isInteger()))
-                                      .bind("conditionVariable")))),
-                                  hasLHS(expression(hasType(isInteger()))
-                                         .bind("conditionBound")))),
+                                  hasLHS(ArrayLHSMatcher),
+                                  hasRHS(ArrayRHSMatcher))),
       hasIncrement(unaryOperator(
           hasOperatorName("++"),
           hasUnaryOperand(declarationReference(to(
-              variable(hasType(isInteger())).bind("incrementVariable"))))))));
+              variable(hasType(isInteger())).bind(IncrementVarName))))))));
+
 
 } // namespace loop_migrate
 } // namespace clang
